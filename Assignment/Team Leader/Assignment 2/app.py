4336004bd4945e93777261592b88f60333fdd4bd
jagadeesh70 <jagankrishna7@gmail.com>
2022-11-25 00:38:14 +0530

    completed second assignment

--- Assignment/Team Leader/Assignment 2/app.py
+++ Assignment/Team Leader/Assignment 2/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PASSWORD'] = 'test1234'
 app.config['MYSQL_DB'] = 'IBMAssign2'
 
 mysql = MySQL(app)
